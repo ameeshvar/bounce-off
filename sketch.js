eda26edaf98fc11297f124fb5d05f987d7b21559
ameeshvar <65909104+ameeshvar@users.noreply.github.com>
2020-06-03 15:29:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -21,10 +21,10 @@ function draw() {
       car.velocityX = car.velocityX * (-1);
       wall.velocityX = wall.velocityX * (-1);
 }  
-  if (car.x - wall.x < wall.width/2 + car.width/2
-  && wall.x - car.x < wall.width/2 + car.width/2) {
-    car.velocityX = car.velocityX * (-1);
-    wall.velocityX = wall.velocityX * (-1);
+  if (car.y - wall.y < wall.width/2 + car.width/2
+  && wall.y - car.y < wall.width/2 + car.width/2) {
+    car.velocityY = car.velocityY* (-1);
+    wall.velocityY = wall.velocityY * (-1);
   }
   drawSprites();
 }
